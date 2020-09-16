1c1cf44aaebde47c53dea327faf1bcab0cbca9a6
h2ljh <h2ljh@naver.com>
2020-09-16 16:12:10 +0900

    POST

--- Node_Hello_3/routes/index.js
+++ Node_Hello_3/routes/index.js
@@ -8,7 +8,6 @@ router.get('/', function(req, res, next)
   let date = moment().format("YYYY-MM-DD");
   let time = moment().format('HH:mm:ss');
   let title = '반갑습니다.'
-
   res.render('index', { title, date, time } );
 });
 
@@ -16,8 +15,8 @@ router.post('/', function(req, res)
 {
   let date = moment().format("YYYY-MM-DD");
   let time = moment().format('HH:mm:ss');
-  let name = req.body.name + "님 반갑습니다.";
-  res.render('index', {name, date, time})
+  let title = req.body.name + "님 반갑습니다.";
+  res.render('index', {title, date, time})
 })
 
 module.exports = router;
